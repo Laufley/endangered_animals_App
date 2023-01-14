21732735657747353a2c4f9aa902c3182bf518c7
Indira Borras <borrasindira@gmail.com>
2023-01-14 17:56:55 +0000

    added_more_stuff

--- js/app.js
+++ js/app.js
@@ -1,34 +1,41 @@
-// document.addEventListener('DOMContentLoaded',() => {
-//     console.log('yep!, it works');
-// })  // dummy test
+document.addEventListener('DOMContentLoaded',() => {
+    const textInput = document.querySelector('#input');
+    textInput.addEventListener('input', handleInput);
 
+    const select = document.querySelector('#select');
+    select.addEventListener('change', handleSelectChange);
+})  
 
-//DOM handling
-// document.addEventListener('DOMContentLoaded', () =>{
-//     const select = document.querySelector('#select');
-//     select.addEventListener('submit', handleSelectChange);
-// })
+//  FUNCTIONS
+const handleInput = function (event) {
+    const output = document.querySelector('#input-result');
+    output.textContent = `The species: ${event.target.value}`;
+};
+            //var animal = handleInput.output
 
+const handleSelectChange = function (event) {
+    const resultParagraph = document.querySelector('#select-result');
+    resultParagraph.textContent = `Lives in: "${event.target.value}".`;
+};
 
+// const button
 
-// //Functions
+// const list = document.querySelector('ul');
+// const newItem = document.createElement('li');
+// newItem.textContent = handleInput.event.target.value + handleSelectChange.event.target.value;
+// list.appendChild(newItem);
 
-// const handleSelectChange = function (event) {
-//     const resultParagraph = document.querySelector('#select-result');
-//     resultParagraph.textContent = `You went with: ${event.target.value}.`;
-// };
+//Creating new item
+
+    // const newListItem = document.createElement("li") // create a new item
+    // newListItem.textContent = "test" // what the new element says
+    // const list = document.querySelector("ul") // selecting the parent of the element, and capturing under a variable called 'list'
+    // list.appendChild(newListItem) // appending the new child to the parent now callew 'list'
 
 
 // ****1.Create a form in HTML with inputs for relevant data DONE
-// 2.When the form is submitted, access the data from the form in the form's submit event object
-// 3.Create a list in HTML
+// ****2.When the form is submitted, access the data from the form in the form's submit event object
+// ****3.Create a list in HTML
 // 4.Append the submitted data to the list
 // 5.Add a "Delete All" <button> which clears all of the list items from the list
-// Note: If you are at all unclear about the process of reading and writing to the DOM, complete the MVP, writing all the code in one function. Understanding the document's methods is the main learning for this homework. Abstracting code into helper functions is secondary.
 
-// const newListItem = document.createElement("li") // create a new item
-//     newListItem.textContent = "Purple" // what the new element says
-//     newListItem.classList.add('purple') // adding the css class to the new item
-//     const list = document.querySelector("ul") // selecting the parent of the element, and capturing under a variable called 'list'
-//     list.appendChild(newListItem) // appending the new child to the parent now callew 'list'
-//     list.insertBefore(newListItem, redItem)
